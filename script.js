121847b358792a13f05245faa88807f7d110434d
Mritunjay Patel <97620757+Mrpatel2319@users.noreply.github.com>
2025-02-01 15:29:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
-    //Write your code here
-
-
+    // Select all <strong> elements
+    const boldWords = document.querySelectorAll('strong');
+    
+    // Change the color of each <strong> element to green
+    boldWords.forEach(word => {
+        word.style.color = 'green';
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
-
+    // Select all <strong> elements
+    const boldWords = document.querySelectorAll('strong');
     
+    // Revert the color of each <strong> element back to black
+    boldWords.forEach(word => {
+        word.style.color = 'black';
+    });
 }
